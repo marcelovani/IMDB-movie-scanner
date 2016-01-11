018f9ff1c28619893c3fac9fde03050b04dc8bdc
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-01-11 11:04:58 +0000

    Updated readme. Added option to specify movies folder

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # IMDB-movie-scanner
+====================
 Python script that scans local folders and updates meta data and cover photos. It can
 send the film info to a CMS using an End point.
 
@@ -6,15 +7,16 @@ Requires IMDBPY
 https://github.com/alberanid/imdbpy
 
 Installation
-- Download IMDBPY
-- Go inside the folder
-- run: sudo ./setup.py
-- Go inside IMDB Movie Scanner
-- run: sudo ./setup.py
-- Edit config.ini
-  - Set your movies folder
+============
+- Download IMDBPY inside a folder called imdbpy. It's advisable to place this folder as a sibbling of IMDB-movie-scanner
+- Go inside the folder and run: sudo ./setup.py [folder]
+  The parameter folder defines where to scan your movies from
 
-Using it
-run: ./scan.py [folder]
+- Go inside IMDB Movie Scanner folder and run: sudo ./setup.py install
+The installation will create a file called config.ini which you can edit and customize your options
+
+
+Usage
+========
+Go inside IMDB-movie-scanner folder and run: ./scan.py [folder]
   - The parameter folder is optional, if blank, it will use the folder in config.ini
-
--- setup.py
+++ setup.py
@@ -1,27 +1,50 @@
 #!/usr/bin/env python
 
-import os, sys
+import os, sys, getopt
 import ConfigParser
 
-# Get current folder
-pwd = os.getcwd()
+def usage(code, msg=''):
+    print 'Usage:'
+    print '  --version: Displays version'
+    print '  --movies-folder: Specify where your movies are located'
+    if msg:
+        print >> sys.stderr, msg
+    sys.exit(code)
 
-# Create config file
-cfgfile = open(pwd + '/config.ini','w')
-config = ConfigParser.RawConfigParser()
+try:
+    opts, args = getopt.getopt(sys.argv[1:], 'ho:v', ['help', 'movies-folder=', 'version'])
+except getopt.GetoptError as err:
+	usage(0)
 
-config.add_section('Library')
-config.set('Library','imdbpy_folder', os.path.realpath(pwd + '/../imdbpy'))
-config.set('Library','imdbpy_limit', '5')
+# parse options
+for option, arg in opts:
+    if option in ('-h', '--movies-folder'):
+    	movies_folder = arg
+    elif option in ('-V', '--version'):
+        print '1.0'
+        sys.exit(0)
 
-config.add_section('Movies')
-config.set('Movies','movies_folder','/Users/marcelovani/Downloads/movies')
-config.set('Movies','file_extensions',['avi', 'dat', 'mp4', 'mkv', 'vob', 'mpeg', 'mpg'])
 
-config.add_section('CMS')
-config.set('CMS','cms_api_url','http://www.example.com')
-config.set('CMS','cms_api_id','admin')
-config.set('CMS','cms_api_key','0000-0000-0000-0000')
+if __name__ == "__main__":
+	# Get current folder
+	pwd = os.getcwd()
 
-config.write(cfgfile)
-cfgfile.close()
+	# Create config file
+	cfgfile = open(pwd + '/config.ini','w')
+	config = ConfigParser.RawConfigParser()
+
+	config.add_section('Library')
+	config.set('Library','imdbpy_folder', os.path.realpath(pwd + '/../imdbpy'))
+	config.set('Library','imdbpy_limit', '5')
+
+	config.add_section('Movies')
+	config.set('Movies','movies_folder', movies_folder)
+	config.set('Movies','file_extensions',['avi', 'dat', 'mp4', 'mkv', 'vob', 'mpeg', 'mpg'])
+
+	config.add_section('CMS')
+	config.set('CMS','cms_api_url','http://www.example.com')
+	config.set('CMS','cms_api_id','admin')
+	config.set('CMS','cms_api_key','0000-0000-0000-0000')
+
+	config.write(cfgfile)
+	cfgfile.close()
